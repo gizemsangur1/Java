0c1164261500ac9997279fc7ce9b4dd8e45f8601
Gizem Şangür <54063806+gizemsangur1@users.noreply.github.com>
2023-03-02 20:21:18 +0300

    Add files via upload

--- sinav.java
+++ sinav.java
@@ -14,22 +14,31 @@ public class sinav {
 		System.out.println("Dersinizi giriniz:");
 		String dersadi =scanner.nextLine();
 		
+		System.out.println("Birinci vize notunuz ortalamanizi yuzde kac etkiliyor");
+		double vize1etki=scanner.nextDouble();
+		
 		System.out.println("1.Vize notunuzu giriniz:");
 		double vizenot1=scanner.nextDouble();
+		
+		System.out.println("Ikinci vize notunuz ortalamanizi yuzde kac etkiliyor");
+		double vize2etki=scanner.nextDouble();
 		
 		System.out.println("2.Vize notunuzu giriniz:");
 		double vizenot2=scanner.nextDouble();
 		
+		
+		double finaletki=100-(vize1etki+vize2etki);
+		
 		System.out.println("Final notunuzu giriniz:");
 		double finalnot=scanner.nextDouble();
 		
-		double ogrencinotu=((vizenot1*30)/100)+((vizenot2*30)/100)+((finalnot*40)/100);
+		double ogrencinotu=((vizenot1*vize1etki)/100)+((vizenot2*vize2etki)/100)+((finalnot*finaletki)/100);
 		
 		if(ogrencinotu<60) {
 			System.out.println(isim+soyisim+" adli ogrenci "+ogrencinotu+" ortalamasi ile "+dersadi+" dersinden kalmistir.");
 		}
 		else {
-			System.out.println(isim+soyisim+" adli ogrenci "+ogrencinotu+" ortalamasi ile "+dersadi+" dersinden gecmistir.");
+			System.out.println(isim+ soyisim+" adli ogrenci "+ogrencinotu+" ortalamasi ile "+dersadi+" dersinden gecmistir.");
 		}
 	}
 
