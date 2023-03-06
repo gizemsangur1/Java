f3ec76de3209c890f25066e552aa45e5899e732c
Gizem Şangür <54063806+gizemsangur1@users.noreply.github.com>
2023-03-06 20:51:08 +0300

    Add files via upload

--- atm.java
+++ atm.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class atm {
 	static int islem;
-	public static void islemsec(int islem) {
+	static int bakiye=1000;
+	public static void islemsec() {
 		Scanner scanneri=new Scanner(System.in);
-		System.out.println("Lutfen yapmak istediginiz islem seciniz.");
+		System.out.println("Lutfen yapmak istediginiz islemi seciniz.");
 		System.out.println("---1)Para yatirma---");
 		System.out.println("---2)Para cekme---");
 		System.out.println("---3)Bakiye sorgulama---");
 		System.out.println("---4)Cikis---");
 		islem=scanneri.nextInt();
 	}
-	public static void main(String[] args) {
-		int bakiye=1000;
+
+	public static void giris(){
 		System.out.println("Lutfen kart numarnizi giriniz.");
 		Scanner scanner = new Scanner(System.in);
 		int kartno=scanner.nextInt();
@@ -28,28 +29,58 @@ public class atm {
 			System.out.println("Sifre yanlis lutfen tekrar gir.");
 			sifre=scanner.nextInt();
 		}
-		islemsec(islem);
-		
-		switch(islem){
-		case 1:
+	}
+	public static void islemyap(){
+		if(islem==1) {
+			Scanner scannerekle=new Scanner(System.in);
 			System.out.println("Ne kadar yatirmak istediginiz miktari soyleyiniz.");
-			int yatirma=scanner.nextInt();
+			int yatirma=scannerekle.nextInt();
 			bakiye=bakiye+yatirma;
 			System.out.println("Yatirma islemiden sonraki miktar."+bakiye);
-			break;
-		case 2:
-			System.out.println("Cekmek istediginiz miktari soyleyiniz");
-			int cekme=scanner.nextInt();
-			bakiye=bakiye-cekme;
-			System.out.println("Cekme islemiden sonraki miktar."+bakiye);
-			break;
-		case 3:
+			System.out.println("Ne islem yapmak istediginizi seciniz.");
+			islemsec();
+			islemyap();
+
+		}
+		else if(islem==2){
+			if(bakiye>0) {
+				Scanner scannercek=new Scanner(System.in);
+				System.out.println("Cekmek istediginiz miktari soyleyiniz");
+				int cekme=scannercek.nextInt();
+				bakiye=bakiye-cekme;
+				System.out.println("Cekme islemiden sonraki miktar."+bakiye);
+				System.out.println("Ne islem yapmak istediginizi seciniz.");
+				islemsec();
+				islemyap();
+			}
+			else {
+				System.out.println("Bakiyeniz 0 oldugu icin daha fazla para cekemezsiniz.");
+				System.out.println("Ne islem yapmak istediginizi seciniz.");
+				islemsec();
+				islemyap();
+			}
+		}
+		else if(islem==3) {
 			System.out.println(bakiye);
-			break;
-		case 4:
+			System.out.println("Ne islem yapmak istediginizi seciniz.");
+			islemsec();
+			islemyap();
+		}
+		else if(islem==4){
 			System.exit(0);
 		}
+		else {
+			System.out.println("Yapmak istediginiz islem gecersiz lutfen gecerli bir islem seciniz.");
+			islemsec();
+			islemyap();
+		}
 		
+	}
+	
+	public static void main(String[] args) {
+		giris();
+		islemsec();
+		islemyap();
 	}
 
 }
